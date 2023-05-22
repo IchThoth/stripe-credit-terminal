a8ce31e6e20df941508e0164e04d8cada0d98ab2
ichthoth <shalomtata334@gmail.com>
2023-05-22 16:15:23 +0100

    initial commit with

---
+++ cmd/web/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+const cssVersion = "1"
+
+type config struct {
+	port int
+	env  string
+	api  string
+	db   struct {
+		dsn string
+	}
+	stripeInfo struct {
+		secret string
+		pubkey string
+	}
+}
+
+type application struct {
+	config        config
+	infoLog       *log.Logger
+	errorLog      *log.Logger
+	templateCache map[string]*template.Template
+	version       string
+}
+
+func (app *application) Server() error {
+	srv := &http.Server{
+		Addr:              fmt.Sprintf(":%d", app.config.port),
+		Handler:           app.routes(),
+		ReadTimeout:       10 * time.Second,
+		ReadHeaderTimeout: 5 * time.Second,
+		WriteTimeout:      5 * time.Second,
+		IdleTimeout:       30 * time.Second,
+	}
+	app.infoLog.Println("starting server in %s mode on port %d", app.config.env, app.config.port)
+
+	return srv.ListenAndServe()
+}
+
+func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "server port to listen on")
+	flag.StringVar(&cfg.env, "env", "development", "Application environment {development|production}")
+	flag.StringVar(&cfg.api, "api", "http://localhost:4001", "URL to api")
+
+	flag.Parse()
+
+	cfg.stripeInfo.secret = os.Getenv("SECRET_KEY")
+	cfg.stripeInfo.secret = os.Getenv("PUBLIC_KEY")
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	tc := make(map[string]*template.Template)
+
+	app := &application{
+		config:        cfg,
+		infoLog:       infoLog,
+		errorLog:      errorLog,
+		templateCache: tc,
+		version:       version,
+	}
+
+	if err := app.Server(); err != nil {
+		app.errorLog.Println(err)
+		log.Fatal(err)
+	}
+}
