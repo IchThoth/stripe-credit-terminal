67edfc6deb1eadbbaedeedc82256f3cf5c549717
ichthoth <shalomtata334@gmail.com>
2023-10-12 05:35:01 +0100

    payment suceeded receipt

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,3 +9,32 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 		app.errorLog.Println(err)
 	}
 }
+
+
+func (app *application) PaymentSucceeded(w http.ResponseWriter, r *http.Request)  {
+	err:= r.ParseForm()
+	if err!= nil {
+		app.errorLog.Println(err)
+	}
+
+	cardHolder := r.Form.Get("cardholder_name")
+	email:= r.Form.Get("email")
+	pi:= r.Form.Get("payment_intent")
+	pc:= r.Form.Get("payment_currency")
+	pa:= r.Form.Get("payment_amount")
+	pm:=r.Form.Get("payment_method")
+
+	data:= make(map[string]interface{})
+	data["cardholder"] = cardHolder
+	data["email"] = email
+	data["pi"] = pi
+	data["pc"] = pc
+	data["pa"] = pa
+	data["pm"] = pm
+
+	if err := app.renderTemplates(w, r, "succeded", &templateData{
+		Data: data,
+	}); err != nil {
+		app.errorLog.Println(err)
+	}
+}
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,6 +10,7 @@ func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
 
 	mux.Get("/home", app.Home)
+	mux.Post("/payment-succeded",app.PaymentSucceeded)
 
 	return mux
 }
