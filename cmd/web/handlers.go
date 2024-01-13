ff10e2a216bb5ab59435d04156e7c18286695192
ichthoth <shalomtata334@gmail.com>
2024-01-13 23:27:07 +0100

    create new transaction and order

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,9 +3,11 @@ package main
 import (
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/ichthoth/stripe-credit-terminal/internal/cards"
+	"github.com/ichthoth/stripe-credit-terminal/internal/models"
 )
 
 func (app *application) PosTerminal(w http.ResponseWriter, r *http.Request) {
@@ -14,18 +16,25 @@ func (app *application) PosTerminal(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func (app *application) HomePage(w http.ResponseWriter, r *http.Request) {
+	if err := app.renderTemplates(w, r, "terminal", &templateData{}); err != nil {
+		app.errorLog.Println(err)
+	}
+}
 func (app *application) PaymentSucceeded(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 	if err != nil {
 		app.errorLog.Println(err)
 	}
 
-	cardHolder := r.Form.Get("cardholder_name")
+	firstName := r.Form.Get("first_name")
+	lastName := r.Form.Get("last_name")
 	email := r.Form.Get("email")
 	paymentintent := r.Form.Get("payment_intent")
 	paymentcurrency := r.Form.Get("payment_currency")
 	paymentamount := r.Form.Get("payment_amount")
 	paymentmethod := r.Form.Get("payment_method")
+	gophimg, _ := strconv.Atoi(r.Form.Get("product_id"))
 
 	card := cards.Card{
 		Secret: app.config.stripeInfo.secret,
@@ -45,9 +54,59 @@ func (app *application) PaymentSucceeded(w http.ResponseWriter, r *http.Request)
 	cardExpiryM := pm.Card.ExpMonth
 	cardExpiryY := pm.Card.ExpYear
 
+	// creates the customer
+	customerID, err := app.SaveCustomer(firstName, lastName, email)
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	app.infoLog.Println(customerID)
+
+	//creates a new transaction
+	amount, err := strconv.Atoi(paymentamount)
+	if err != nil {
+		app.errorLog.Println(err)
+	}
+
+	txn := models.Transaction{
+		Amount:              amount,
+		Currency:            paymentcurrency,
+		LastFour:            lastFour,
+		BankReturnCode:      pi.Charges.Data[0].ID,
+		ExpiryMonth:         int(cardExpiryM),
+		ExpiryYear:          int(cardExpiryY),
+		TransactionStatusID: 2,
+	}
+
+	txnID, err := app.SaveTransaction(txn)
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	//creating a new order
+	ord := models.Order{
+		ImageID:       gophimg,
+		TransactionID: txnID,
+		CustomerID:    customerID,
+		StatusID:      1,
+		Quantity:      1,
+		Amount:        amount,
+		CreatedAt:     time.Now(),
+		UpdatedAt:     time.Now(),
+	}
+
+	_, err = app.SaveOrder(ord)
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
 	data := make(map[string]interface{})
-	data["cardholder"] = cardHolder
 	data["email"] = email
+	data["fn"] = firstName
+	data["ln"] = lastName
 	data["pi"] = paymentintent
 	data["pc"] = paymentcurrency
 	data["pa"] = paymentamount
@@ -62,6 +121,40 @@ func (app *application) PaymentSucceeded(w http.ResponseWriter, r *http.Request)
 	}); err != nil {
 		app.errorLog.Println(err)
 	}
+}
+
+func (app *application) SaveCustomer(firstName, lastName, email string) (int, error) {
+	customer := models.Customer{
+		FirstName: firstName,
+		LastName:  lastName,
+		Email:     email,
+	}
+
+	id, err := app.DB.InsertCustomer(customer)
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
+
+}
+
+func (app *application) SaveTransaction(txn models.Transaction) (int, error) {
+	id, err := app.DB.InsertTransaction(txn)
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
+}
+
+func (app *application) SaveOrder(ord models.Order) (int, error) {
+	id, err := app.DB.InsertOrder(ord)
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
 }
 
 func (app *application) ChargeOnce(w http.ResponseWriter, r *http.Request) {
